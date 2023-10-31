c95121e623b584c925352919f2042ce0a5e9dc38
Serg <freekey@list.ru>
2023-10-31 11:58:22 +0400

    Stub and tests for add

--- src/main/java/ru/sergjavacode/PhoneBook.java
+++ src/main/java/ru/sergjavacode/PhoneBook.java
@@ -1,4 +1,10 @@
 package ru.sergjavacode;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class PhoneBook {
+ int add(String name, String phoneNumber){
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.sergjavacode.PhoneBook;
+
 public class PhoneBookTest {
+    @Test
+    public void addTest(){
+        PhoneBook phoneBook=new PhoneBook();
+        Assertions.assertEquals(1,phoneBook.add("Коля","26-25-26"));
+        Assertions.assertEquals(2,phoneBook.add("Юля","56-78-99"));
+        Assertions.assertEquals(2,phoneBook.add("Коля","26-29-26"));
+    }
+
+
 }
